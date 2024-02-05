244219433da384adfdb4ae5302e8db2fd53b343e
surya selvam <surya.selvam@thebluespire.com>
2024-02-05 17:04:02 +0530

    Update product page

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -2,7 +2,10 @@
 import React, { useState } from "react";
 import { useRouter } from 'next/navigation';
 import "../styles/product.css"
+import "../styles/mainService.css";
 import "../../../public/Nature.jpg"
+import { OurservicesHeader } from "../../../utils/info";
+import Image from "next/image";
 
 type Product = {
   id: string;
@@ -10,7 +13,7 @@ type Product = {
   description: string;
   image: string;
   detail: string;
-  content: { id: string; name: string; description:string,image:string}[];
+  content: { id: string; name: string; description: string, image: string }[];
 };
 
 const Products = () => {
@@ -20,34 +23,34 @@ const Products = () => {
       id: "1",
       name: "E-Permits / Registrations",
       image: "/E-com.jpg",
-      content : [
-        {id:"1",name : "Concrete Batch Plant (CBP) ",description:" Mobile Concrete Batching Plant have the world's latest Twin Shaft Mixer technology for fast and homogeneous mixing with fully automatic PLC control system with SCADA/HMI. Its Rugged and compact design makes it suitable for handling all types of concrete with high accuracy and minimum maintenance.",image: "/Nature.jpg"},
-        {id:"2",name : "Crushing & Screening Plant (C&S) ",description:"A crushing plant setup is a series of machines that are designed to take large pieces of concrete, asphalt or rock and break them into smaller pieces. The crushed material can then be used for construction, landscaping, and other purposes.KYB-Conmat Mobile Concrete Batching Plant have the world's",image: "/E-com.jpg"},
-        {id:"3",name : "Hot-Mix Asphalt Plant (HMAP)",description:"HMA is the traditional process for constructing asphalt pavements. HMA is manufactured in a central mixing facility (typically called a hot mix plant) and consists of high quality aggregate and asphalt cement. The two are heated and mixed while hot to completely coat the aggregate with asphalt cement.",image: "/E-com.jpg"},
-        {id:"4",name : "Construction GP",description:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae, ullam voluptas sunt numquam dolorum accusantium dolor praesentium porro eligendi ad natus repellendus! Eveniet alias, ex neque itaque deserunt provident minima?",image: "/Nature.jpg"},
-        {id:"5",name : "Multi-Sector GP",description:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae, ullam voluptas sunt numquam dolorum accusantium dolor praesentium porro eligendi ad natus repellendus! Eveniet alias, ex neque itaque deserunt provident minima?",image: "/E-com.jpg",},
-        {id:"6",name : "Description text goes here",description:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae, ullam voluptas sunt numquam dolorum accusantium dolor praesentium porro eligendi ad natus repellendus! Eveniet alias, ex neque itaque deserunt provident minima?",image: "/Nature.jpg"},
-      
+      content: [
+        { id: "1", name: "Concrete Batch Plant (CBP) ", description: " Mobile Concrete Batching Plant have the world's latest Twin Shaft Mixer technology for fast and homogeneous mixing with fully automatic PLC control system with SCADA/HMI. Its Rugged and compact design makes it suitable for handling all types of concrete with high accuracy and minimum maintenance.", image: "/cbp.jpg" },
+        { id: "2", name: "Crushing & Screening Plant (C&S) ", description: "A crushing plant setup is a series of machines that are designed to take large pieces of concrete, asphalt or rock and break them into smaller pieces. The crushed material can then be used for construction, landscaping, and other purposes.KYB-Conmat Mobile Concrete Batching Plant have the world's", image: "/E-com.jpg" },
+        { id: "3", name: "Hot-Mix Asphalt Plant (HMAP)", description: "HMA is the traditional process for constructing asphalt pavements. HMA is manufactured in a central mixing facility (typically called a hot mix plant) and consists of high quality aggregate and asphalt cement. The two are heated and mixed while hot to completely coat the aggregate with asphalt cement.", image: "/E-com.jpg" },
+        { id: "4", name: "Construction GP", description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae, ullam voluptas sunt numquam dolorum accusantium dolor praesentium porro eligendi ad natus repellendus! Eveniet alias, ex neque itaque deserunt provident minima?", image: "/Nature.jpg" },
+        { id: "5", name: "Multi-Sector GP", description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae, ullam voluptas sunt numquam dolorum accusantium dolor praesentium porro eligendi ad natus repellendus! Eveniet alias, ex neque itaque deserunt provident minima?", image: "/E-com.jpg", },
+        { id: "6", name: "Description text goes here", description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae, ullam voluptas sunt numquam dolorum accusantium dolor praesentium porro eligendi ad natus repellendus! Eveniet alias, ex neque itaque deserunt provident minima?", image: "/Nature.jpg" },
+
       ],
       detail:
         "It all begins with an idea. Maybe you want to launch a business. Whatever it is, the way you tell your story online can make all the difference",
 
       description:
         "E-Permit is a web-based solution that includes complete life cycle of different types of permits provided by State Transport Authority or Regional Transport Authority.",
-        
+
     },
     {
       id: "2",
       name: "E-Compliance Reporting",
       image: "/E-com.jpg",
-      content : [
-        {id:"1",name : "Rock Products Annual Compliance",description:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae, ullam voluptas sunt numquam dolorum accusantium dolor praesentium porro eligendi ad natus repellendus! Eveniet alias, ex neque itaque deserunt provident minima?",image: "/E-com.jpg"},
-        {id:"2",name : "Air Quality Excess Emissions ",description:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae, ullam voluptas sunt numquam dolorum accusantium dolor praesentium porro eligendi ad natus repellendus! Eveniet alias, ex neque itaque deserunt provident minima?",image: "/Nature.jpg"},
-        {id:"3",name : "Air Quality Excess Emissions ",description:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae, ullam voluptas sunt numquam dolorum accusantium dolor praesentium porro eligendi ad natus repellendus! Eveniet alias, ex neque itaque deserunt provident minima?",image: "/Nature.jpg"},
-        {id:"4",name : "surya",description:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae, ullam voluptas sunt numquam dolorum accusantium dolor praesentium porro eligendi ad natus repellendus! Eveniet alias, ex neque itaque deserunt provident minima?",image: "/E-com.jpg"},
-        {id:"5",name : "raj",description:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae, ullam voluptas sunt numquam dolorum accusantium dolor praesentium porro eligendi ad natus repellendus! Eveniet alias, ex neque itaque deserunt provident minima?",image: "/Nature.jpg"},
-        {id:"6",name : "kumar",description:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae, ullam voluptas sunt numquam dolorum accusantium dolor praesentium porro eligendi ad natus repellendus! Eveniet alias, ex neque itaque deserunt provident minima?",image: "/E-com.jpg"},
-      
+      content: [
+        { id: "1", name: "Rock Products Annual Compliance", description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae, ullam voluptas sunt numquam dolorum accusantium dolor praesentium porro eligendi ad natus repellendus! Eveniet alias, ex neque itaque deserunt provident minima?", image: "/E-com.jpg" },
+        { id: "2", name: "Air Quality Excess Emissions ", description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae, ullam voluptas sunt numquam dolorum accusantium dolor praesentium porro eligendi ad natus repellendus! Eveniet alias, ex neque itaque deserunt provident minima?", image: "/Nature.jpg" },
+        { id: "3", name: "Air Quality Excess Emissions ", description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae, ullam voluptas sunt numquam dolorum accusantium dolor praesentium porro eligendi ad natus repellendus! Eveniet alias, ex neque itaque deserunt provident minima?", image: "/Nature.jpg" },
+        { id: "4", name: "surya", description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae, ullam voluptas sunt numquam dolorum accusantium dolor praesentium porro eligendi ad natus repellendus! Eveniet alias, ex neque itaque deserunt provident minima?", image: "/E-com.jpg" },
+        { id: "5", name: "raj", description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae, ullam voluptas sunt numquam dolorum accusantium dolor praesentium porro eligendi ad natus repellendus! Eveniet alias, ex neque itaque deserunt provident minima?", image: "/Nature.jpg" },
+        { id: "6", name: "kumar", description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae, ullam voluptas sunt numquam dolorum accusantium dolor praesentium porro eligendi ad natus repellendus! Eveniet alias, ex neque itaque deserunt provident minima?", image: "/E-com.jpg" },
+
       ],
       detail:
         "It all begins with an idea. Maybe you want to launch a business. Whatever it is, the way you tell your story online can make all the difference",
@@ -57,11 +60,11 @@ const Products = () => {
     {
       id: "3",
       name: "Other Applications",
-      image: "/E-com.jpg",
-      content : [
-        {id:"1",name : "Operator Certification Online Portal",description:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae, ullam voluptas sunt numquam dolorum accusantium dolor praesentium porro eligendi ad natus repellendus! Eveniet alias, ex neque itaque deserunt provident minima?",image: "/Nature.jpg"},
-        {id:"2",name : "Open Burn Permit Online Portal",description:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae, ullam voluptas sunt numquam dolorum accusantium dolor praesentium porro eligendi ad natus repellendus! Eveniet alias, ex neque itaque deserunt provident minima?",image: "/E-com.jpg"},
-        {id:"3",name : "Vehicle Emissions Testing Exemptions",description:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae, ullam voluptas sunt numquam dolorum accusantium dolor praesentium porro eligendi ad natus repellendus! Eveniet alias, ex neque itaque deserunt provident minima?",image: "/Nature.jpg"},
+      image: "/Nature.jpg",
+      content: [
+        { id: "1", name: "Operator Certification Online Portal", description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae, ullam voluptas sunt numquam dolorum accusantium dolor praesentium porro eligendi ad natus repellendus! Eveniet alias, ex neque itaque deserunt provident minima?", image: "/Nature.jpg" },
+        { id: "2", name: "Open Burn Permit Online Portal", description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae, ullam voluptas sunt numquam dolorum accusantium dolor praesentium porro eligendi ad natus repellendus! Eveniet alias, ex neque itaque deserunt provident minima?", image: "/E-com.jpg" },
+        { id: "3", name: "Vehicle Emissions Testing Exemptions", description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae, ullam voluptas sunt numquam dolorum accusantium dolor praesentium porro eligendi ad natus repellendus! Eveniet alias, ex neque itaque deserunt provident minima?", image: "/Nature.jpg" },
       ],
       detail:
         "It all begins with an idea. Maybe you want to launch a business. Whatever it is, the way you tell your story online can make all the difference",
@@ -71,44 +74,110 @@ const Products = () => {
   ]);
 
   return (
-    <div>  
-    <div className="px-20 py-5">
-      <h2 className="text-4xl font-bold text-black-500 text-center ">Products</h2>
-      <p className="text-3xl font-medium text-black my-4">What we offer</p>
-      {products.map((product, index) => (
-        <div key={index} className="product-card">
-          <div>
-              <img
-                src={product.image}
-                alt="e permit"
-                className="image-card"
-              />
+    <div>
+      <div className="px-20 py-5">
+        <h2 className="text-4xl font-bold text-black-500 text-center ">Products</h2>
+        {/* <p className="text-3xl font-medium text-black my-4">What we offer</p> */}
+        <div className='top flex flex-row-reverse flex-wrap gap-10'>
+          <div className="ServicesHeader">
+            <h1 className='title'>What we offer</h1>
+            <p>{OurservicesHeader}</p>
           </div>
-          <div className="w-full flex flex-col justify-between">
-            <h3 className="text-black font-bold text-2xl mb-2">
-              {product.name}
-            </h3>
-            <p className="text-black font-normal text-base mb-3">
-              {product.description}
-            </p>
-            {/* <p className="text-black font-normal text-base mb-3">
-              {product.detail}
-            </p> */}
-            <div className="flex justify-end">
-              <button
-                className="view-button"
-                onClick={() => {
-                  router.push(`/products/view?id=${product?.id}`);
-                  localStorage.setItem("product", JSON.stringify(product));
-                }}
-              >
-                View Product
-              </button>
+          <div className="Header">
+            <Image src={"/tech2.jpg"} alt="image1 " width={500} height={700} className='tech2'></Image>
+          </div>
+        </div>
+
+
+
+        {products.map((product, index) => (
+          <div className='top flex flex-wrap items-stretch gap-5 product-box'>
+            <div className="productHeader flex flex-col justify-between p-10">
+              <div>
+                <h1 className='titleHead'>{product?.name}</h1>
+                <p>{product?.description}</p>
+              </div>
+              <div>
+                <button
+                  className="view-button"
+                  onClick={() => {
+                    router.push(`/products/view?id=${product?.id}`);
+                    localStorage.setItem("product", JSON.stringify(product));
+                  }}
+                >
+                  View Product
+                </button>
+              </div>
+
+            </div>
+            <div className="Header">
+              <Image src={product?.image} alt="image1 " width={500} height={700} className='tech2'></Image>
+            </div>
+          </div>
+
+          // <div key={index} className="product-card">
+          //   <div>
+          //     <img
+          //       src={product.image}
+          //       alt="e permit"
+          //       className="image-card"
+          //     />
+          //   </div>
+          //   <div className="w-full flex flex-col justify-between">
+          //     <h3 className="text-black font-bold text-2xl mb-2">
+          //       {product.name}
+          //     </h3>
+          //     <p className="text-black font-normal text-base mb-3">
+          //       {product.description}
+          //     </p>
+          //     {/* <p className="text-black font-normal text-base mb-3">
+          //     {product.detail}
+          //   </p> */}
+          //     <div className="flex justify-end">
+          //       <button
+          //         className="view-button"
+          //         onClick={() => {
+          //           router.push(`/products/view?id=${product?.id}`);
+          //           localStorage.setItem("product", JSON.stringify(product));
+          //         }}
+          //       >
+          //         View Product
+          //       </button>
+          //     </div>
+          //   </div>
+          // </div>
+        ))}
+      </div>
+
+      {/* <div className="container">
+        <div className="flex flex-wrap">
+          <div className="lg:w-1/2 xl:w-5/12">
+            <div className="text-container p-6">
+              <h2 className="text-3xl font-bold mb-4">Branding & Strategy</h2>
+              <p className="mb-4">
+                To build a solid foundation for your online shop you need a strong brand and a bulletproof strategy. You can rely on our experience to create a beautiful and efficient online shop.
+              </p>
+              <div id="details" className="basic-1">
+                <ul className="list-disc list-inside space-y-2">
+                  <li>Delivering powerful brands for popular shops</li>
+                  <li>Creating beautiful concepts and evocative logos</li>
+                  <li>Preparing complete branding guides and styles</li>
+                  <li>Speedy delivery of marketing design assets</li>
+                </ul>
+              </div>
+            </div>
+          </div>
+          <div className="lg:w-1/2 xl:w-7/12">
+            <div className="image-container p-6">
+              <img className="w-100% h-50%" src="E-com.jpg" alt="alternative" />
             </div>
           </div>
         </div>
-      ))}
-    </div>
+      </div> */}
+
+     
+
+     
     </div>
   );
 };
--- src/app/products/view/page.tsx
+++ src/app/products/view/page.tsx
@@ -23,7 +23,7 @@ const View = () => {
     }
   };
 
- useEffect(() => {
+  useEffect(() => {
     // Check if localStorage is available before using it
     if (typeof window !== "undefined") {
       const productData = localStorage.getItem("product");
@@ -44,17 +44,17 @@ const View = () => {
       </h2>
 
       {/* Use grid and create a 3x3 grid for the cards */}
-      <div className="grid grid-cols-3 gap-0.5 p-2 m-2 over py-2">
+      <div className="flex flex-wrap items-center justify-between py-2">
         {product?.content.map((item: ProductItem, index: number) => (
           <div className="single-card" key={index}>
-            <div className="card-img">
-              <img 
-              src={item.image}
-               alt={item.name}
-               className="object-contain" />
+            <div className="card-img ">
+              <img
+                src={item.image}
+                alt={item.name}
+                className="object-cover w-full h-full" />
             </div>
             <p className="text-black font-bold text-2xl justify-center font-poppins">{item?.name}</p>
-            <p className="text-black font-normal text-base font-poppins-regular">{item?.description}</p>
+            <p className="card-des">{item?.description}</p>
           </div>
         ))}
       </div>
