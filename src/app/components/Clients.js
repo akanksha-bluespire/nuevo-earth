37f7f480070903282b8b74856bc2061397860877
SrilekhaMS <saisrilekha.marthi@thebluespire.com>
2024-02-05 10:34:21 +0530

    home page width bug fixed

--- src/app/components/Clients.js
+++ src/app/components/Clients.js
@@ -11,8 +11,8 @@ const clientsData = [
 
 const Clients = () => {
   return (
-    <div className='mx-[3%] bg-gradient-to-r'>
-      <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3'>
+    <div className='mx-[3%] bg-gradient-to-r '>
+      <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5'>
         {clientsData.map((client, index) => (
           <div key={index} className=' bg-white  rounded-md flex flex-col items-center justify-center'>
             <img src={client.logoSrc} alt={`${client.name} Logo`}  className='mb-2'/>
--- src/app/components/HomePage.js
+++ src/app/components/HomePage.js
@@ -4,19 +4,18 @@ import Header from './Header';
 
 const HomePage = () => {
   return (
-    <div className='h-screen w-full bg-no-repeat bg-cover bg-center relative' style={{
+    <div className='h-screen w-screen bg-no-repeat bg-cover bg-center relative' style={{
       backgroundImage: "url('homebg.jpg')",
-      backgroundSize: 'cover',
-      backgroundPosition: 'center center',
+      width: '100%'
     }}>
       <Header />
       <div className='absolute top-0 left-0 w-full h-full' style={{
-        backgroundColor: 'rgba(0, 0, 0, 0.6)', 
+        backgroundColor: 'rgba(0, 0, 0, 0.6)',
         zIndex: 1,
       }}>
-        <div className='flex justify-center items-center h-[90%]'>
+        <div className='flex justify-center items-center h-screen w-full'>
           <h2 className='text-5xl text-white text-center font-bold font-serif leading-normal'>
-            Innovative Solutions for <br/> Environmental Compliance
+            Innovative Solutions for <br /> Environmental Compliance
           </h2>
         </div>
       </div>
